06279569c3e514e9c6c8eebe3d5f4372b9813d2f
Armando Prado <luis.prado@hemoeco.com>
2022-02-12 18:34:39 -0600

    fix: added a screenshot of Kata2

--- Kata2/env/Scripts/Activate.ps1
+++ Kata2/env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Armando\Documents\LaunchX\Actividad2\env"
+$env:VIRTUAL_ENV="C:\Users\Armando\Documents\LaunchX\Python(Semana1)\OnBoarding-LaunchX-Python\Kata2\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
